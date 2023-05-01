905d642ba5285f5f181c8726f6aa93c36d57fe06
Giuliano Frieri <gfrieri@uninorte.edu.co>
2023-04-30 21:58:34 -0500

    Detalle menor en restaurant

--- src/restaurant/restaurant.model.js
+++ src/restaurant/restaurant.model.js
@@ -3,7 +3,6 @@ import { Schema, model } from 'mongoose';
 const restaurantSchema = new Schema(
   {
     name: { type: String, required: true },
-    rating: { type: Array, required: false },
     address: { type: String, required: true },
     categories: {
       type: [String],
